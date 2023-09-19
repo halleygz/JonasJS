aacf2b3cbff184a8abc3842f43b3c0fafb793205
halleygz <halleluya2013idone@gmail.com>
2023-09-19 17:04:17 +0300

    modal fix on script.js and refactoring

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnOpen = document.querySelectorAll('.show-modal');
+const closeBtn = document.querySelector('.close-modal');
+const close = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const open = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < btnOpen.length; i++)
+  btnOpen[i].addEventListener('click', open);
+
+closeBtn.addEventListener('click', close);
+overlay.addEventListener('click', close);
+const pressed = function (e) {
+  if (!modal.classList.contains('hidden') && e.key === 'Escape') close();
+};
+document.addEventListener('keydown', pressed);
